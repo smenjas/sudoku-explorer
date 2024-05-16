84af06b8e1fba195770e924296337f9d194332a5
Ben Thornton <smenjas@users.noreply.github.com>
2024-05-16 10:17:29 -0700

    Check solutions.

--- client/index.js
+++ client/index.js
@@ -1,3 +1,62 @@
+function checkValue(set, value) {
+    if (value < 1 || value > 9) {
+        return false;
+    }
+    if (set.has(value)) {
+        return false;
+    }
+    set.add(value);
+    return true;
+}
+
+function checkRow(board, r) {
+    const set = new Set();
+    for (const c in board[r]) {
+        if (checkValue(set, board[r][c]) === false) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function checkColumn(board, c) {
+    const set = new Set();
+    for (const r in board) {
+        if (checkValue(set, board[r][c]) === false) {
+            return false;
+        }
+    }
+    return true;
+}
+
+function checkBlock(board, r, c) {
+    const blockRows = findBlock(r);
+    const blockColumns = findBlock(c);
+    const set = new Set();
+    for (const ri in blockRows) {
+        for (const ci in blockColumns) {
+            if (checkValue(set, board[ri][ci]) === false) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
+// Check that every number follows the rules.
+function checkSolution(board) {
+    for (const r in board) {
+        for (const c in board[r]) {
+            if (checkRow(board, r) === false
+                || checkColumn(board, c) === false
+                || checkBlock(board, r, c) === false) {
+                return false;
+            }
+        }
+    }
+    return true;
+}
+
 // Return a 9x9 array filled with any value.
 function fillSudoku(value) {
     const board = [];
@@ -94,7 +153,8 @@ function createSudoku() {
 
 // Render a 9x9 array of sudoku digits as an HTML table.
 function renderSudoku(board) {
-    let html = '<table id="sudoku">';
+    const valid = checkSolution(board);
+    let html = `<table id="sudoku" class="${valid}">`;
     for (const r in board) {
         html += '<tr>';
         for (const c in board[r]) {
