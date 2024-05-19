65f573f1a5b033cc190ad056499b549dca4b74bf
Ben Thornton <smenjas@users.noreply.github.com>
2024-05-19 14:07:51 -0700

    Refactor clue pattern functions, to allow for other pattern types.

--- pattern.js
+++ pattern.js
@@ -1,49 +1,57 @@
 import { fillSudoku, getRandomCandidate } from './sudoku.js';
 
-// Generate an array of 5 bits (binary digits).
-// There must be 1 or 2 ones, and thus 3 or 4 zeros.
+// Generate an array of random bits (binary digits).
 // A one means "show this clue," and a zero means "hide this clue."
-function getPentet() {
+function getRandomBits(size, max = size, min = 0) {
     let shown = 0;
-    const pentet = Array(5).fill(0);
-    while (shown < 1) {
-        for (let i = 0; i < 5; i++) {
+    const bitfield = Array(size).fill(0);
+    while (shown < min) {
+        for (let i = 0; i < size; i++) {
             const show = Math.round(Math.random());
-            pentet[i] = show;
+            bitfield[i] = show;
             shown += show;
-            // Restrict clues to 2 per pentet (4 per row).
-            if (shown > 1) {
+            if (shown === max) {
                 break;
             }
         }
     }
-    return pentet;
+    return bitfield;
 }
 
-// Generate a 9x9 array of bits (binary digits), to show or hide each clue.
-// A one means "show this clue," and a zero means "hide this clue."
-function getCluePattern() {
-    const pentets = [];
-    for (let i = 0; i < 5; i++) {
-        pentets.push(getPentet());
-    }
-    // Restrict clues to 4 per column.
-    for (let c = 0; c < 5; c++) {
+// Generate an array of 5 bits (binary digits).
+// There must be 1 or 2 ones, and thus 3 or 4 zeros.
+function getPentet() {
+    return getRandomBits(5, 2, 1);
+}
+
+// Restrict the number of ones in a 2D array of bitfields.
+function restrictCluesPerColumn(pattern, max) {
+    const columns = pattern[0].length;
+    for (let c = 0; c < columns; c++) {
         let sum = 0;
-        for (let r = 0; r < 5; r++) {
-            sum += pentets[r][c];
+        for (let r = 0; r < pattern.length; r++) {
+            sum += pattern[r][c];
         }
-        while (sum > 2) {
-            const rows = new Set([...Array(5).keys()]);
+        while (sum > max) {
+            const rows = new Set([...Array(pattern.length).keys()]);
             const r = getRandomCandidate(rows);
-            if (pentets[r][c] === 0) {
+            if (pattern[r][c] === 0) {
                 continue;
             }
-            pentets[r][c] = 0;
+            pattern[r][c] = 0;
             sum--;
             rows.delete(r);
         }
     }
+}
+
+// Generate a clue pattern that has dual orthogonal reflection symmetry.
+function getReflectionPattern() {
+    const pentets = [];
+    for (let i = 0; i < 5; i++) {
+        pentets.push(getPentet());
+    }
+    restrictCluesPerColumn(pentets, 2);
     const pattern = fillSudoku(0);
     for (let r = 0; r < 5; r++) {
         for (let c = 0; c < 5; c++) {
@@ -62,6 +70,16 @@ function getCluePattern() {
         }
     }
     return pattern;
+}
+
+// Generate a 9x9 array of bits (binary digits), to show or hide each clue.
+// A one means "show this clue," and a zero means "hide this clue."
+function getCluePattern() {
+    const styles = new Set([
+        getReflectionPattern,
+    ]);
+    const style = getRandomCandidate(styles);
+    return style();
 }
 
 function sum2d(matrix) {
